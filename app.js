e0f7266a69ea833ab4da0ba7fc223b09251ffdd8
Eduardo Lamarche <lamarcheeduardo@gmail.com>
2022-11-16 23:20:56 -0400

    new update

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const msg = document.getElementById('msg')
 //Game Object
 const myGame = {
     gameBoard: [],
-    playerOne: {name: 'Eduardo', value: 'X'},
-    playerTwo: {name: 'Miguel', value: 'O'},
+    setPlayersName: function(){
+        this.playerOne.name = prompt('Player one name: ')
+        this.playerTwo.name = prompt('Player two name: ')
+    },
+    playerOne: {name: '', value: 'X'},
+    playerTwo: {name: '', value: 'O'},
     winner: false,
     setWinner: function(){
-        return this.winner = true
+        return this.winner = true 
     },
     turn: true //create a function to asign this value.
 }
@@ -62,71 +66,109 @@ function render() {
    
 }
 
+function winningCombination(){ 
+    [
+    0, 1, 2,
+    0, 3, 6,
+    0, 1, 8,
+    2, 4, 6,
+    3, 4, 5,
+    6, 7, 8,
+    6, 4, 2,
+]
+}
+
 function checkStatus(){
     if(myGame.gameBoard[0] == 'X' && myGame.gameBoard[1] == 'X' && myGame.gameBoard[2] == 'X') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerOne.name} wins`
     } else if(myGame.gameBoard[0] == 'O' && myGame.gameBoard[1] == 'O' && myGame.gameBoard[2] == 'O') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerTwo.name} wins`
     } else if(myGame.gameBoard[0] == 'O' && myGame.gameBoard[3] == 'O' && myGame.gameBoard[6] == 'O') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerTwo.name} wins`
     } else if(myGame.gameBoard[0] == 'X' && myGame.gameBoard[3] == 'X' && myGame.gameBoard[6] == 'X'){
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerOne.name} wins`
     } else if(myGame.gameBoard[0] == 'X' && myGame.gameBoard[4] == 'X' && myGame.gameBoard[8] == 'X') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerOne.name} wins`
     } else if(myGame.gameBoard[0] == 'O' && myGame.gameBoard[4] == 'O' && myGame.gameBoard[8] == 'O') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerTwo.name} wins`
     } else if(myGame.gameBoard[3] == 'O' && myGame.gameBoard[4] == 'O' && myGame.gameBoard[5] == 'O') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerTwo.name} wins`
     } else if(myGame.gameBoard[3] == 'X' && myGame.gameBoard[4] == 'X' && myGame.gameBoard[5] == 'X') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerOne.name} wins`
     } else if(myGame.gameBoard[6] == 'X' && myGame.gameBoard[7] == 'X' && myGame.gameBoard[8] == 'X') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerOne.name} wins`
     } else if(myGame.gameBoard[6] == 'O' && myGame.gameBoard[7] == 'O' && myGame.gameBoard[8] == 'O') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerTwo.name} wins`
     } else if(myGame.gameBoard[1] == 'O' && myGame.gameBoard[4] == 'O' && myGame.gameBoard[7] == 'O') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerTwo.name} wins`
     } else if(myGame.gameBoard[1] == 'X' && myGame.gameBoard[4] == 'X' && myGame.gameBoard[7] == 'X') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerOne.name} wins`
     } else if(myGame.gameBoard[2] == 'X' && myGame.gameBoard[5] == 'X' && myGame.gameBoard[9] == 'X') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerOne.name} wins`
     } else if(myGame.gameBoard[2] == 'O' && myGame.gameBoard[5] == 'O' && myGame.gameBoard[9] == 'O') {
         setTimeout(() => {
             myGame.setWinner()
         }, 100)
+        msg.innerText = `${myGame.playerTwo.name} wins`
+    } else if(myGame.gameBoard[2] == 'O' && myGame.gameBoard[4] == 'O' && myGame.gameBoard[6] == 'O') {
+        setTimeout(() => {
+            myGame.setWinner()
+        }, 100)
+        msg.innerText = `${myGame.playerTwo.name} wins`
+    } else if(myGame.gameBoard[2] == 'X' && myGame.gameBoard[4] == 'X' && myGame.gameBoard[6] == 'X') {
+        setTimeout(() => {
+            myGame.setWinner()
+        }, 100)
+        msg.innerText = `${myGame.playerOne.name} wins`
     } 
 }
 
 
 function init(){
     if(myGame.winner === true){
-        msg.innerText = 'Welcome';
+        if(myGame.winner)
+        msg.innerText = `${myGame.pl}`;
     } else {
+        myGame.setPlayersName();
         msg.innerText = `${myGame.playerOne.name} turn`
         selectedSquares();
     }
